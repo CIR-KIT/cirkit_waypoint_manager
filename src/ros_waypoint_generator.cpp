29a154eb9d7063ddd57ec7a67b9977bd9fd751a8
Arita Yuta <k104009y@mail.kyutech.jp>
2016-09-30 21:29:31 +0900

    add waypoint load function

--- src/ros_waypoint_generator.cpp
+++ src/ros_waypoint_generator.cpp
@@ -60,19 +60,53 @@ public:
     waypoint_box_count_ = 0;
     server.reset( new interactive_markers::InteractiveMarkerServer("cube") );
   }
-  
+
+  void load(std::string waypoint_file)
+  {
+    const int rows_num = 7; // x, y, z, Qx, Qy, Qz, Qw
+    boost::char_separator<char> sep("," ,"", boost::keep_empty_tokens);
+    std::ifstream ifs(savefilename.c_str());
+    std::string line;
+
+    while(ifs.good()){
+      getline(ifs, line);
+      if(line.empty()){ break; }
+      tokenizer tokens(line, sep);
+      std::vector<double> data;
+      tokenizer::iterator it = tokens.begin();
+      for(; it != tokens.end() ; ++it){
+        std::stringstream ss;
+        double d;
+        ss << *it;
+        ss >> d;
+        data.push_back(d);
+      }
+      if(data.size() != rows_num){
+        ROS_ERROR("Row size is mismatch!!");
+        return;
+      }else{
+        geometry_msgs::PoseWithCovariance new_pose;
+        new_pose.pose.position.x = data[0];
+        new_pose.pose.position.y = data[1];
+        new_pose.pose.position.z = data[2];
+        new_pose.orientation = tf::Quaternion(data[3], data[4], data[5], data[6]);
+        makeWayPointMarker(new_pose);
+      }
+    }
+  }
+
   double calculateDistance(geometry_msgs::PoseWithCovariance new_pose)
   {
     return sqrt(pow(new_pose.pose.position.x - last_pose_.pose.position.x, 2)
                 + pow(new_pose.pose.position.y - last_pose_.pose.position.y, 2));
   }
-  
+
   void getRPY(const geometry_msgs::Quaternion &q,
               double &roll,double &pitch,double &yaw){
     tf::Quaternion tfq(q.x, q.y, q.z, q.w);
     tf::Matrix3x3(tfq).getRPY(roll, pitch, yaw);
   }
-  
+
   double calculateAngle(geometry_msgs::PoseWithCovariance new_pose)
   {
     double yaw, pitch, roll;
@@ -85,10 +119,6 @@ public:
   InteractiveMarkerControl& makeWaypointMarkerControl(InteractiveMarker &msg)
   {
     InteractiveMarkerControl control;
- 
-    // control.orientation_mode = InteractiveMarkerControl::VIEW_FACING;
-    // control.interaction_mode = InteractiveMarkerControl::MOVE_ROTATE;
-    //control.independent_marker_orientation = true;
     control.orientation.w = 1;
     control.orientation.x = 0;
     control.orientation.y = 1;
@@ -102,7 +132,7 @@ public:
     control.orientation.z = 0;
     control.interaction_mode = InteractiveMarkerControl::MOVE_PLANE;
     msg.controls.push_back(control);
-    
+
     Marker marker;
     marker.type = Marker::CUBE;
     marker.scale.x = msg.scale*0.5;
@@ -112,16 +142,15 @@ public:
     marker.color.g = 0.80;
     marker.color.b = 0.02;
     marker.color.a = 1.0;
-    
+
     control.markers.push_back(marker);
     control.always_visible = true;
     msg.controls.push_back(control);
 
     return msg.controls.back();
-    
+
   }
-  
-  
+
   void makeWaypointMarker(const geometry_msgs::PoseWithCovariance new_pose)
   {
     InteractiveMarker int_marker;
@@ -135,13 +164,13 @@ public:
     int_marker.description = s.str();
 
     makeWaypointMarkerControl(int_marker);
-    
+
     server->insert(int_marker);
     server->setCallback(int_marker.name, &processFeedback);
     server->applyChanges();
     waypoint_box_count_++;
   }
-  
+
   void addWaypoint(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& amcl_pose)
   {
     double diff_dist = calculateDistance(amcl_pose->pose);
@@ -152,6 +181,7 @@ public:
       last_pose_ = amcl_pose->pose;
     }
   }
+
   void run()
   {
     ros::spin();
@@ -162,7 +192,7 @@ private:
   ros::Subscriber odom_sub_;
 
   geometry_msgs::PoseWithCovariance last_pose_;
-  
+
   double dist_th_;
   double yaw_th_;
   int waypoint_box_count_;
@@ -174,5 +204,30 @@ int main(int argc, char** argv)
 {
   ros::init(argc, argv, "waypoint_generator");
   WaypointGenerator generator;
+
+  boost::program_options::options_description desc("Options");
+  desc.add_options()
+    ("help", "Print help message")
+    ("load", boost::program_options::value<std::string>(), "waypoint filename");
+
+  boost::program_options::variables_map vm;
+  try {
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+    boost::program_options::notify(vm);
+    if( vm.count("help") ){
+      std::cout << "This is waypoint generator node" << std::endl;
+      std::cerr << desc << std::endl;
+      return 0;
+    }
+    if (vm.count("load")) {
+      generator.load(vm["load"].as<std::string>());
+    }
+  } catch (boost::program_options::error& e) {
+    std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+    std::cerr << desc << std::endl;
+    return -1;
+  }
+
   generator.run();
+  return 0;
 }
