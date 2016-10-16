7b4e91d711f989c3a83377563f66ac8275448310
Arita Yuta <k104009y@mail.kyutech.jp>
2016-10-17 01:29:37 +0900

    waypoint navigatorとりあえず完成。コンパイルまでは通る。

--- waypoint_navigator/src/waypoint_navigator.cpp
+++ waypoint_navigator/src/waypoint_navigator.cpp
@@ -66,88 +66,45 @@ class WaypointNavigator
 public:
   WaypointNavigator() : ac_("move_base", true), rate_(100)
   {
-    robot_behavior_state_ = RobotBehaviors::State::INIT_NAV;
-    stasis = INITNAVI;
-    target_num = 0;
+    target_waypoint_index_ = 0;
+    robot_behavior_state_ = RobotBehaviors::INIT_NAV;
     std::string filename;
 
     ros::NodeHandle n("~");
-    n.param<std::string>("waypointsfile", filename, 
-                         ros::package::getPath("waypoint_navigator") 
+    n.param<std::string>("waypointsfile", filename,
+                         ros::package::getPath("waypoint_navigator")
                          + "/waypoints/default.csv");
-    n.param("start_target_num", target_num, 0);
-    ROS_INFO("[Start target num] : %d", target_num);
+
+    n.param("dist_thres_to_target_object", dist_thres_to_target_object_, 1.5);
     ROS_INFO("[Waypoints file name] : %s", filename.c_str());
 
     ROS_INFO("Reading Waypoints.");
     readWaypoint(filename.c_str());
-   
     ROS_INFO("Waiting for action server to start.");
-    ac.waitForServer();
+    ac_.waitForServer();
   }
 
-  void sendNewGoal()
+  void sendNewGoal(geometry_msgs::Pose pose)
   {
     move_base_msgs::MoveBaseGoal goal;
-    goal = goals[target_num];
-    // Need boost::bind to pass in the 'this' pointer
+    goal.target_pose.pose = pose;
     goal.target_pose.header.frame_id = "map";
     goal.target_pose.header.stamp = ros::Time::now();
-    ac.sendGoal(goal,
-                boost::bind(&MyMoveBaseClient::doneCb, this, _1),
-                boost::bind(&MyMoveBaseClient::activeCb, this),
-                boost::bind(&MyMoveBaseClient::feedbackCb, this, _1));
-    stasis = NAVIGATING;
-  }
-
-  void doneCb(const actionlib::SimpleClientGoalState& state)
-  {
-    ROS_INFO("Finished in state [%s]", state.toString().c_str());
-    stasis = INITNAVI;
-  }
-
-  // Called once when the goal becomes active
-  void activeCb()
-  {
-    ROS_INFO("Goal just went active");
-  }
-
-  // Called every time feedback is received for the goal.
-  void feedbackCb(const move_base_msgs::MoveBaseFeedbackConstPtr& feedback)
-  {
-    //ROS_INFO("[X]: %f [Y]: %f", feedback->base_position.pose.position.x, feedback->base_position.pose.position.y);
-    double now_x = feedback->base_position.pose.position.x;
-    double now_y = feedback->base_position.pose.position.y;
-    double tar_x = goals[target_num-1].target_pose.pose.position.x;
-    double tar_y = goals[target_num-1].target_pose.pose.position.y;
-    double dist = calculateDistance(tar_x, tar_y, now_x, now_y);
-    // ROS_INFO("[num ]: %d", target_num);
-    // ROS_INFO("[tarX]: %lf [tarY]: %lf", tar_x, tar_y);
-    // ROS_INFO("[nowX]: %lf [nowY]: %lf", now_x, now_y);
-    if(dist <= 2.0){
-      stasis = NEARGOAL;
-      ROS_INFO("Reached WayPT[%d]!!! [Dist] : %lf",target_num, dist);
-    }else{
-      ROS_INFO("Search WayPT[%d] [Dist]: %lf",target_num, dist);
-    }
-  }
-
-  int getStasis(){
-    return stasis;
+    ac_.sendGoal(goal);
+    now_goal_ = goal.target_pose.pose;
   }
 
   void cancelGoal(){
     ROS_INFO("cancelGoal() is called !!");
-    ac.cancelGoal();
+    ac_.cancelGoal();
   }
 
   int readWaypoint(std::string filename)
   {
-    const int rows_num = 8; // x, y, z, Qx,Qy,Qz,Qw, searching_area
+    const int rows_num = 9; // x, y, z, Qx,Qy,Qz,Qw, area_type, reach_threshold
     boost::char_separator<char> sep("," ,"", boost::keep_empty_tokens);
     std::ifstream ifs(filename.c_str());
     std::string line;
-	
     while(ifs.good()){
       getline(ifs, line);
       if(line.empty()){ break; }
@@ -165,102 +122,176 @@ public:
         ROS_ERROR("Row size is mismatch!!");
         return -1;
       }else{
-        move_base_msgs::MoveBaseGoal goal;
-        goal.target_pose.pose.position.x    = data[0];
-        goal.target_pose.pose.position.y    = data[1];
-        goal.target_pose.pose.position.z    = data[2];
-        goal.target_pose.pose.orientation.x = data[3];
-        goal.target_pose.pose.orientation.y = data[4];
-        goal.target_pose.pose.orientation.z = data[5];
-        goal.target_pose.pose.orientation.w = data[6];
-        goals.push_back(goal);
-        search_area_.push_back((int)data[7]);
-        ROS_INFO("[%d]--> [X]: %f [Y]: %f", (int)goals.size(), goal.target_pose.pose.position.x, goal.target_pose.pose.position.y);
+        move_base_msgs::MoveBaseGoal waypoint;
+        waypoint.target_pose.pose.position.x    = data[0];
+        waypoint.target_pose.pose.position.y    = data[1];
+        waypoint.target_pose.pose.position.z    = data[2];
+        waypoint.target_pose.pose.orientation.x = data[3];
+        waypoint.target_pose.pose.orientation.y = data[4];
+        waypoint.target_pose.pose.orientation.z = data[5];
+        waypoint.target_pose.pose.orientation.w = data[6];
+        waypoints_.push_back(WayPoint(waypoint, (int)data[7], data[8]));
       }
     }
-    finish_num = (int)goals.size();
     return 0;
   }
 
-  void detectTargetObjectCallback()
+  void detectTargetObjectCallback(const jsk_recognition_msgs::BoundingBoxArray::ConstPtr &target_objects_ptr)
   {
+    target_objects_ = *target_objects_ptr;
+  }
+
+  WayPoint getNextWaypoint()
+  {
+    WayPoint next_waypoint = waypoints_[target_waypoint_index_];
+    target_waypoint_index_++;
+    return next_waypoint;
+  }
+
+  bool isFinalGoal()
+  {
+    if (target_waypoint_index_ == (int)waypoints_.size()) {
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  bool isAlreadyApproachedToTargetObject(jsk_recognition_msgs::BoundingBox target_object)
+  {
+    for (int i = 0; i < approached_target_objects_.boxes.size(); ++i) {
+      double dist = calculateDistance(target_object.pose,
+                                      approached_target_objects_.boxes[i].pose);
+      if (dist < 3.0) { // しきい値はパラメータサーバで設定できるようにする
+        return true;
+      }
+    }
+    return false;
+  }
+
+  double calculateDistance(geometry_msgs::Pose a,geometry_msgs::Pose b)
+  {
+     sqrt(pow((a.position.x - b.position.x), 2.0) +
+          pow((a.position.y - b.position.y), 2.0));
+  }
+
+  // 探索対象へのアプローチの場合
+  void setNextGoal(jsk_recognition_msgs::BoundingBox target_object,
+                   double threshold)
+  {
+    reach_threshold_ = threshold;
+    // 探索対象へのアプローチの場合はアプローチ時のロボットの姿勢を計算する必要がある
+    // まだ出来てない。
+    approached_target_objects_.boxes.push_back(target_object);//探索済みに追加
+    this->sendNewGoal(target_object.pose);
+  }
+
+  // 通常のwaypointの場合
+  void setNextGoal(WayPoint waypoint)
+  {
+    reach_threshold_ = waypoint.reach_threshold_;
+    this->sendNewGoal(waypoint.goal_.target_pose.pose);
+  }
+
+  double getReachThreshold()
+  {
+    return reach_threshold_;
+  }
+
+  geometry_msgs::Pose getRobotCurrentPosition()
+  {
+    // tfを使ってロボットの現在位置を取得する
+    tf::StampedTransform transform;
+    geometry_msgs::Pose pose;
+    try {
+      listener_.lookupTransform("/map", "/base_link", ros::Time(0), transform);
+    } catch (tf::TransformException ex) {
+      ROS_ERROR("%s", ex.what());
+    }
+    pose.position.x = transform.getOrigin().x();
+    pose.position.y = transform.getOrigin().y();
+    
+    return pose;
+  }
+
+  geometry_msgs::Pose getNowGoalPosition()
+  {
+    return now_goal_;
   }
   
   void run()
   {
-    
-    int ret = 0;
-    robot_behavior_state_ = RobotBehaviors::State::INIT_NAV;
+    robot_behavior_state_ = RobotBehaviors::INIT_NAV;
     while(ros::ok()){
-      next_waypoint = this->getNextWaypoint();
-      if (next_waypoint.is_search_area) { // 次のwaypointが探索エリアがどうか判定
-        if(target_objects_.size() > 0){ // 探索対象が見つかっているか
-          for (i = 0; i < target_objects_.size(); ++i) {
-            if (! this->isAlreadyApproached(target_objects_[i])) { // 探索対象にまだアプローチしていなかったら
-              this->setNextGoal(target_objects_[i], dist_thres_to_apploach_); // 探索対象を次のゴールに設定して
-              this->addAlreadyApproached(target_objects_[i]); // 探索対象の位置をアプローチ済みに追加
-              robot_behavior_state_ = RobotBehaviors::State::DETECT_TARGET_NAV;
+      WayPoint next_waypoint = this->getNextWaypoint();
+      if (next_waypoint.isSearchArea()) { // 次のwaypointが探索エリアがどうか判定
+        if(target_objects_.boxes.size() > 0){ // 探索対象が見つかっているか
+          for (int i = 0; i < target_objects_.boxes.size(); ++i) {
+            if (! this->isAlreadyApproachedToTargetObject(target_objects_.boxes[i])) { // 探索対象にまだアプローチしていなかったら
+              this->setNextGoal(target_objects_.boxes[i], dist_thres_to_target_object_); // 探索対象を次のゴールに設定
+              robot_behavior_state_ = RobotBehaviors::DETECT_TARGET_NAV;
               break;
             }
           }
         }else // 探索エリアだが探索対象がいない
         {
           this->setNextGoal(next_waypoint);
-          robot_behavior_state_ = RobotBehaviors::State::WAYPOINT_NAV;
+          robot_behavior_state_ = RobotBehaviors::WAYPOINT_NAV;
         }
       }else // 探索エリアではない
       {
         this->setNextGoal(next_waypoint);
-        robot_behavior_state_ = RobotBehaviors::State::WAYPOINT_NAV;
+        robot_behavior_state_ = RobotBehaviors::WAYPOINT_NAV;
       }
       while(1)
       {
-        robot_current_position = this->getRobotCurrentPosition(); // 現在のロボットの座標
-        next_goal_position = this->getNextGoalPosition(); // 現在目指している座標
-        distance_to_goal = this->calculateDistance(robot_current_position,
-                                                   next_goal_position);
+        geometry_msgs::Pose robot_current_position = this->getRobotCurrentPosition(); // 現在のロボットの座標
+        geometry_msgs::Pose now_goal_position = this->getNowGoalPosition(); // 現在目指している座標
+        double distance_to_goal = this->calculateDistance(robot_current_position,
+                                                          now_goal_position);
         // ここでスタック判定してもいいかもしれない。
         // 一定時間進んでない、もしくは一定回数後も移動距離が変化していないなら
         // ゴールをキャンセルして次のwaypointに進むとか？
-        if(distance_to_goal < this->getGoalThreshold()) // 目標座標までの距離がしきい値になれば
+        if(distance_to_goal < this->getReachThreshold()) // 目標座標までの距離がしきい値になれば
         {
-          robot_behavior_state_ = RobotBehaviors::State::REACHED_GOAL;
+          robot_behavior_state_ = RobotBehaviors::REACHED_GOAL;
           break;
         }
         rate_.sleep();
         ros::spinOnce();
       }
-      if (robot_behavior_state_ == RobotBehaviors::State::REACHED_GOAL) { //waypointに到達してて
+      if (robot_behavior_state_ == RobotBehaviors::REACHED_GOAL) { //waypointか探索対象に到達してて
         if (this->isFinalGoal()) { // そのwaypointが最後だったら
           this->cancelGoal(); // ゴールをキャンセルして終了
           return;
         }
       }
-      rate.sleep();
+      rate_.sleep();
       ros::spinOnce();
     }// while(ros::ok())
   }
-  
+
 private:
   MoveBaseClient ac_;
   RobotBehaviors::State robot_behavior_state_;
-  int target_num_;
-  int finish_num_;
   ros::Rate rate_;
-  std::vector<move_base_msgs::MoveBaseGoal> goals_;
-  std::vector<int> search_area_;
+  std::vector<WayPoint> waypoints_;
   ros::NodeHandle nh_;
-  // ros::ServiceClient client;
-  // sound::sound_service srv;
+  tf::TransformListener listener_;
+  int target_waypoint_index_; // 次に目指すウェイポイントのインデックス
+  jsk_recognition_msgs::BoundingBoxArray target_objects_; //探索対象
+  jsk_recognition_msgs::BoundingBoxArray approached_target_objects_; //アプローチ済みの探索対象
+  double dist_thres_to_target_object_; // 探索対象にどれだけ近づいたらゴールとするか
+  double reach_threshold_; // 今セットされてるゴール（waypointもしくは探索対象）へのしきい値
+  geometry_msgs::Pose now_goal_; // 現在目指しているゴールの座標
+
 };
 
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "waypoint_navigator");
-  
-  
+
   WaypointNavigator waypoint_navigator;
- 
   waypoint_navigator.run();
  
   return 0;
