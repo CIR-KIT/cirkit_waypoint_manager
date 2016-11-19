1f19bdae773aa4a54d7466420451d500f63dd2fe
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-11-19 14:36:16 +0900

    Reformat ros_colored_msg.h

--- waypoint_navigator/include/ros_colored_msg.h
+++ waypoint_navigator/include/ros_colored_msg.h
@@ -1,10 +1,8 @@
 // Defining your own ROS stream color
 // https://gist.github.com/artivis/51c6c9436481b16cab2f
 // auther : artivis
-namespace pc
-{
-  enum PRINT_COLOR
-  {
+namespace pc {
+  enum PRINT_COLOR {
     BLACK,
     RED,
     GREEN,
@@ -16,10 +14,8 @@ namespace pc
     ENDCOLOR
   };
 
-  std::ostream& operator<<(std::ostream& os, PRINT_COLOR c)
-  {
-    switch(c)
-    {
+  std::ostream& operator<<(std::ostream& os, PRINT_COLOR c) {
+    switch(c) {
       case BLACK    : os << "\033[1;30m"; break;
       case RED      : os << "\033[1;31m"; break;
       case GREEN    : os << "\033[1;32m"; break;
